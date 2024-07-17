de89a68c8097c6ce7e4ed580153545b515cff27e
erayerm <73521213+erayerm@users.noreply.github.com>
2024-07-18 00:13:31 +0300

    small changes

--- app/components/profile/RecentActivity.jsx
+++ app/components/profile/RecentActivity.jsx
@@ -7,7 +7,7 @@ function RecentActivity() {
     return (
         <div className="mt-5">
             <div className="flex justify-between w-full border-b pb-1 mb-2 border-b-[rgba(136,153,170,0.5)]">
-                <h2 className="">RECENT ACTIVITY</h2>
+                <Link href={`/${user.username}/activity/`} className="hover:text-hover-blue">RECENT ACTIVITY</Link>
                 <Link href={`/${user.username}/films/`} className="text-[11px] text-[#678] hover:text-hover-blue">ALL</Link>
             </div>
             <div className="flex justify-between">
--- app/components/profile/RecentReviews.jsx
+++ app/components/profile/RecentReviews.jsx
@@ -1,4 +1,3 @@
-import MovieCard from "../MovieCard"
 import { movies } from "../../mock/movies.js"
 import { user } from "../../mock/user.js"
 import { reviews } from "../../mock/reviews.js"
@@ -14,8 +13,8 @@ function RecentReviews() {
     return (
         <div className="mt-5">
             <div className="flex justify-between w-full border-b pb-1 mb-2 border-b-[rgba(136,153,170,0.5)]">
-                <h2 className="">RECENT REVIEWS</h2>
-                <Link href={`/${user.username}/films/`} className="text-[11px] text-[#678] hover:text-hover-blue">MORE</Link>
+                <Link href={`/${user.username}/films/reviews/by/added/`} className="hover:text-hover-blue">RECENT REVIEWS</Link>
+                <Link href={`/${user.username}/films/reviews/by/added/`} className="text-[11px] text-[#678] hover:text-hover-blue">MORE</Link>
             </div>
             <div className="w-full flex flex-col">
                 {reviews.map((item, index) => {
